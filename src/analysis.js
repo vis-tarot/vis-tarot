84580be98a47764824c543c70f6301164a9a8aff
Michael Correll <michael.a.correll@gmail.com>
2019-12-31 15:01:25 -0800

    Thinking about how to model the minor arcana

--- src/analysis.js
+++ src/analysis.js
@@ -1,10 +1,45 @@
+//Pentacles: outlier strength
+// Visualization: histogram with red outlier glyph
+
+//Wands: non-normality
+// Visualization: histogram
+
+//Cups: correlation
+// Visualization: scatterplot
+
+//Swords: missing data or nulls
+// Visualization: histogram with nulls/missing data on the side
 
 
-//Cluster points, return the points that are furtherest away from
-var outlierStrength = function(data){
 
+//largest z score difference between values
+var outlierStrength = function(data,accessor){
+  //largest z scores
+  const stdev = dl.stdev(data,accessor);
+  const min = dl.min(data,accessor);
+  const max = dl.max(data,accessor);
+  const mean = dl.mean(data,accessor);
+  return stdev===0 ? 0 : Math.max(Math.abs(min-mean)/stdev,Math.max(max-mean)/stdev);
 }
 
+//
+
 var profileFields = function(data){
-  
+  const summary = dl.summary(data);
+  //returns array of summaries
+  //field
+  //unique
+  //count
+  //valid
+  //missing
+  //distinct
+  //min
+  //max
+  //mean
+  //stdev
+  //median
+  //q1
+  //q3
+  //modeskew
+
 }
